de429b3750c06d0c7cc77cfa2a30b89f49bac0e4
Henry Pratama <suryadi_h@worksap.co.jp>
2017-07-24 18:49:47 +0800

    Basic responses

---
+++ index.js
@@ -0,0 +1,6 @@
+const { Composer } = require('micro-bot');
+const app = new Composer();
+
+app.command('/start', (ctx) => ctx.reply('Welcome!'));
+app.hears('hi', ({ reply }) => reply('Hey there!'));
+app.on('sticker', ({ reply }) => reply('👍'));
